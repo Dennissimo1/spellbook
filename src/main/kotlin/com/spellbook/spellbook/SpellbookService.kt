27cb3add2d31ce21196dbfe18328be1728b5335d
dbiljard <dbiljard@bol.com>
2025-09-15 16:21:47 +0200

    Add exclamation mark

--- src/main/kotlin/com/spellbook/spellbook/SpellbookService.kt
+++ src/main/kotlin/com/spellbook/spellbook/SpellbookService.kt
@@ -10,6 +10,6 @@ import java.util.concurrent.Callable
 @CommandLine.Command(name ="spellbook", mixinStandardHelpOptions = true, version = ["spellbook v0.1"], description = ["spellbook"], subcommands = [List::class, Del::class, Add::class ])
 class SpellbookService: Callable<String> {
     override fun call(): String? {
-        TODO("Not yet implemented")
+        TODO("Not yet implemented!")
     }
 }
